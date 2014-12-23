513eadcd4cb959784db660aa515f8e17b56cecb6
Anosike Osifo <osifo@dealdey.com>
2014-12-23 20:56:14 +0100

    assignment with indentation

--- 23/bin/main.rb
+++ 23/bin/main.rb
@@ -3,7 +3,7 @@ include FileOperation
 
 fileOps = FileOps.new
 
-fileOps.read_from_file("csv_read_input.csv")
+fileOps.read_write("csv_read_input.csv")
 
 # fileOps.read_by_column("my_new_file.csv")
 
--- 23/lib/file_operation.rb
+++ 23/lib/file_operation.rb
@@ -2,75 +2,62 @@ module FileOperation
 	class FileOps
 	require 'csv'
 
-		
-		def write_to_file(file_name)
-			csv_file = File.open("/home/osifo/dev/playground/ruby/#{file_name}", "wb")
-			# puts csv_file.path
-			CSV.open("#{csv_file.path}", "wb") do |content|
-				content << ["Lastname", "Firstname", "Email Address"]
-				content << ["Anosike", "Borderless", "osifo@dealdey.com"]
+		def check_freq(array)
+			freq = Hash.new(0) 
+			array.each do |elem|
+				if freq.keys.grep(/#{elem}/i)
+					(freq[elem] = freq[elem] + 1)
+				else
+					freq[elem] = 1
+				end
 			end
+			return freq
+		end
+
+		def pluralize(array)
+			freq_result = check_freq(array)
+			array.each do |elem|
+				elem << "s" if freq_result[elem] > 1
+			end
+
+			# puts "pluralized array: #{array}"
+		end
+
+		def read_write(file_name)
+			from_file = read_from_file(file_name)
+			headers = []
+			from_file.each do |header|
+				headers << header[" Designation"].to_s.strip!
+			end
+			headers = pluralize(headers).uniq! #check for freq and pluralize accordingly
+			write_to_file(from_file, headers)
+
 		end
 
 		def read_from_file(file_name)
 			# csv_file = File.open("/home/osifo/dev/playground/ruby/#{file_name}")
 			file_dir = "/home/osifo/dev/trainings/basicruby/23/lib/#{file_name}"
 			from_file = CSV.read("#{file_dir}", headers: true)
-
-			# puts from_file
-			headers = []
-			from_file.each do |header|
-				headers << header[" Designation"].to_s.strip!
-			end
-			# puts headers
 			
-			# from_file.headers.sort.each do |header|
-			# 	p header
-			# end
+		end
 
-			from_file.each do |row|
+
+
+	
+
+		def write_to_file(csv_file, headers)
+			file_dir = "/home/osifo/dev/trainings/basicruby/23/lib/csv_output.csv"
+
+			File.open("#{file_dir}", "w+") do |content|
 				headers.each do |header|
-					puts "#{row["Name"]} (EmpId:#{row[" EmpId"]})" if row[" Designation"].to_s.strip == header
-				end			
+					content.puts "#{header}\n"
+					csv_file.each do |row|
+						if row[" Designation"].to_s.strip == header
+							content.puts "#{row["Name"]} (EmpId:#{row[" EmpId"]})\n"
+						end
+					end		
+				end	
 			end
-			
-			# from_file.headers.sort.each do |header|
-			# 	p header.strip
-			# 	p from_file["#{header}"]
-			# end
-
-		end
-
-		def read_by_column(file_name)
-			file_dir = "/home/osifo/dev/playground/ruby/#{file_name}"
-			column_data = []
-			count = 0
-			# CSV.foreach("#{file_dir}") do |row|
-			# 	column_data[count] = row[count]
-			# 	count += 1
-			# 	puts row.inspect
-			# end
-
-			CSV.foreach("#{file_dir}") do |row|
-				# column_data << "#{row[0]} : #{row[1]} : #{row[2]}"
-				column_data << row[1]
-				count += 1
-				
-			end
-			puts column_data.inspect
-			# puts column_data
-		end
-
-		def write_to_file(read_source)
-			file_dir = "/home/osifo/dev/playground/ruby/"
-			# read_source = "file_dir#{read_source}"
-
-			source = read_from_file("file_dir#{read_source}")
-			puts source.inspect
-
-			# CSV.open("#{file_dir}/new_write.txt") do |content|
-
-			# end
 		end
 
 	end
