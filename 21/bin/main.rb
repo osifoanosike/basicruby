c8cda6698b5c817949bc269c76e57a80c8d93f02
Anosike Osifo <osifo@dealdey.com>
2015-01-09 09:05:32 +0100

    coreected regex, added error message

--- 21/bin/main.rb
+++ 21/bin/main.rb
@@ -1,11 +1,16 @@
 require_relative '../lib/fixnum.rb'
 
 puts "Enter a value to get its factorial, negative numbers result in errors"
-input = gets.chomp
 
-if !input.match(/\d+/) #if entry isnt an integer
-  puts "Non-integer value entered .Try again"
-else
-  puts "Factorial of #{input}: #{input.to_i.factorial}"
+
+begin
+	input = Integer(gets.chomp)
+
+	if !input.match(/^[0-9]+$/) #if entry isnt an integer
+	  puts "Non-integer value entered .Try again"
+	else
+	  puts "Factorial of #{input}: #{input.to_i.factorial}"
+	end
+rescue ArgumentError => e
+	puts  "Valued entered not an integer \n#{e.backtrace.inspect}"
 end
-
