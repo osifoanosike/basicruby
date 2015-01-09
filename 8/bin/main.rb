379a0490133e95b000b101d1bc2b97e76aeba4a8
Anosike Osifo <osifo@dealdey.com>
2015-01-09 13:55:59 +0100

    RRemove redundant array assignment

--- 8/bin/main.rb
+++ 8/bin/main.rb
@@ -5,5 +5,4 @@ arr = gets.chomp.split(',').map(&:to_i)
 puts "What power do you want raise the elements to?"
 pow = Integer(gets.chomp)
 
-arr = Array.new(arr)
 puts "The Power Array is: #{arr.power(pow)}"
