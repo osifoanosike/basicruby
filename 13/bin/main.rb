f9f42faad32bbc63a7fe0540a39359bfbf10a4a1
Anosike Osifo <osifo@dealdey.com>
2015-01-12 09:41:53 +0100

    Add negative number vaidation in factorial logic

--- 13/bin/main.rb
+++ 13/bin/main.rb
@@ -3,7 +3,7 @@ require_relative '../lib/fixnum.rb'
 puts "Finding Factorial: enter a integer value.. "
 
 input = gets.chomp
-if input.match(/^[0-9]+$/)
+if input.match(/^\d+$/)
   result = input.to_i.factorial
   puts "facorial of #{input}: #{result}"
 else
--- 13/lib/fixnum.rb
+++ 13/lib/fixnum.rb
@@ -1,6 +1,11 @@
 class Fixnum
   def factorial
-    return 1 if self == 0
-    (1..self).inject(:*)
+    if self < 0
+    	raise "Value must be a postive integer"
+    elsif self = 0
+    	1
+    else
+    	(1..self).inject(:*)
+    end
   end
 end
