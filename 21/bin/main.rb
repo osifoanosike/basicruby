ee9cffd2d2be02358680745cca5a5383709dc324
Anosike Osifo <osifo@dealdey.com>
2015-01-19 08:37:35 +0100

    update logic and exception handling

--- 21/bin/main.rb
+++ 21/bin/main.rb
@@ -4,13 +4,13 @@ puts "Enter a value to get its factorial, negative numbers result in errors"
 
 
 begin
-	input = Integer(gets.chomp)
+	input = gets.chomp
 
-	if !input.match(/^[0-9]+$/) #if entry isnt an integer
-	  puts "Non-integer value entered .Try again"
+	if input.match(/^\D+$/) #if entry isnt an integer
+	  puts "Invalid string entry. Try again"
 	else
 	  puts "Factorial of #{input}: #{input.to_i.factorial}"
 	end
 rescue ArgumentError => e
-	puts  "Valued entered not an integer \n#{e.backtrace.inspect}"
+	puts  "An error occurred: #{e.message} \n#{e.backtrace.inspect}"
 end
