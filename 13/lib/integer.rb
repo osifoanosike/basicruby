d42c7cf0b7e9f47b63a131c5937867d57a97949d
Anosike Osifo <osifo@dealdey.com>
2015-01-21 18:56:51 +0100

    Update logic

---
+++ 13/lib/integer.rb
@@ -0,0 +1,11 @@
+class Integer
+  def factorial
+    if self < 0
+      raise "Value must be a postive integer"
+    elsif self == 0
+      1
+    else
+      (1..self).inject(:*)
+    end
+  end
+end
