5b9c40be4bc0b22ae37a7db7903db05127c859fa
Anosike Osifo <osifo@dealdey.com>
2015-01-21 09:05:37 +0100

    Fixe rescue logic to catch appropriate exception

--- 21/bin/main.rb
+++ 21/bin/main.rb
@@ -1,10 +1,17 @@
 require_relative '../lib/fixnum.rb'
+require_relative '../lib/bad_argument.rb'
 
 puts "Enter a value to get its factorial, negative numbers result in errors"
 
-input = gets.chomp
-if !input.match(/^\d+$/) #if entry isnt an integer
-  puts "Invalid string entry. Try again"
-else
-  puts "Factorial of #{input}: #{input.to_i.factorial}"
+
+begin
+	input = gets.chomp
+
+	if input.match(/\D+/) #if entry isnt an integer
+	  puts "Invalid string entry. Try again"
+	else
+	  puts "Factorial of #{input}: #{input.to_i.factorial}"
+	end
+rescue BadArgumentError => e
+	puts  "An error occurred: #{e.message}"
 end
