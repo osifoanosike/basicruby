fab1d91c98a8584afcc990ba2ae22bdbdf001c6c
Anosike Osifo <osifo@dealdey.com>
2015-01-21 09:50:40 +0100

    Correct regex and rescue code

--- 21/bin/main.rb
+++ 21/bin/main.rb
@@ -7,11 +7,11 @@ puts "Enter a value to get its factorial, negative numbers result in errors"
 begin
 	input = gets.chomp
 
-	if input.match(/\D+/) #if entry isnt an integer
+	if !input.match(/^[+-]?\d+$/) #if entry isnt an integer
 	  puts "Invalid string entry. Try again"
 	else
 	  puts "Factorial of #{input}: #{input.to_i.factorial}"
 	end
-rescue BadArgumentError => e
+rescue BadArgument => e
 	puts  "An error occurred: #{e.message}"
 end
