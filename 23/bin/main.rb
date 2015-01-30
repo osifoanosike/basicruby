2782a0ac0fae4e4e6f7c2a34a3b76b659f034afb
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-01-30 12:54:13 +0100

    Optimize code

--- 23/bin/main.rb
+++ 23/bin/main.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/file_operation.rb'
 
 begin
-  fileOps = FileOperation.new
+  file_ops = FileOperation.new
   puts "Enter the name of csv file you want to read from"
   file_name = gets.chomp
 
