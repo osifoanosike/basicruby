3b076a8c3a643abba3a392358cc856e69d0e5032
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-01-30 12:58:39 +0100

    Optimize code

--- 23/bin/main.rb
+++ 23/bin/main.rb
@@ -5,7 +5,7 @@ begin
   puts "Enter the name of csv file you want to read from"
   file_name = gets.chomp
 
-  fileOps.read_write(file_name)
+  file_ops.read_write(file_name)
 rescue Exception => e
   puts "Oops! something has gone wrong: #{e.message}\n#{e.backtrace.inspect}"
 end
